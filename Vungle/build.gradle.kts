af333cb51458e977c7b634bc8769d91d76ea85a7
Jian Chen <quantumsix492@gmail.com>
2022-07-26 15:34:03 -0700

    revert gradle

--- Vungle/build.gradle.kts
+++ Vungle/build.gradle.kts
@@ -1,23 +1,12 @@
 plugins {
     id("signing")
     id("maven-publish")
-    id("com.android.library")
 }
-apply(plugin = "com.android.library")
-
-android {
-    defaultConfig {
-        compileSdkVersion(31)
-        buildConfigField("int", "VERSION_CODE", "06110000")
-        buildConfigField("String", "VERSION_NAME", "\"6.11.0.0\"");
-    }
-}
-
 
 private val versionMajor = 6
-private val versionMinor = 12
+private val versionMinor = 11
 private val versionPatch = 0
-private val versionAdapterPatch = 0
+private val versionAdapterPatch = 1
 
 val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
 val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
@@ -25,11 +14,49 @@ val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000
 val libraryArtifactId by extra("vungle-adapter")
 val libraryGroupId by extra("com.applovin.mediation")
 
+var libraryVersions = rootProject.extra["versions"] as Map<*, *>
+
+android.defaultConfig.versionCode = libraryVersionCode
+android.defaultConfig.versionName = libraryVersionName
+
 dependencies {
+    implementation("com.vungle:publisher-sdk-android:${libraryVersions["vungle"]}")
+}
 
-//    implementation("com.vungle:publisher-sdk-android:6.11.0")
-
-    //TODO: Replace this with maven repo as above when available
-    implementation("com.github.Vungle:vungle-android-sdk:6.12.0-QA2")
-    implementation("com.applovin:applovin-sdk:+@aar")
+publishing {
+    publications {
+        create<MavenPublication>(extra["publicationName"] as String) {
+            //The publication doesn't know about our dependencies, so we have to manually add them to the pom
+            pom.withXml {
+                asNode().apply {
+                    appendNode("name", libraryArtifactId)
+                    appendNode("description", "Vungle adapter for AppLovin MAX mediation")
+                    appendNode("url", "https://www.applovin.com/")
+                    appendNode("licenses")
+                        .appendNode("license").apply {
+                            appendNode("name", "AppLovin Corporation Mediation Adapter EULA")
+                            appendNode("url", "https://www.applovin.com/eula")
+                        }
+                    appendNode("scm").apply {
+                        appendNode("connection", "scm:git:github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
+                        appendNode("developerConnection", "scm:git:ssh://github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
+                        appendNode("url", "https://github.com/AppLovin/AppLovin-MAX-SDK-Android")
+                    }
+                    appendNode("developers")
+                        .appendNode("developer").apply {
+                            appendNode("name", "AppLovin")
+                            appendNode("url", "https://www.applovin.com")
+                        }
+                    // Add Vungle to list of dependencies.
+                    appendNode("dependencies")
+                        .appendNode("dependency").apply {
+                            appendNode("groupId", "com.vungle")
+                            appendNode("artifactId", "publisher-sdk-android")
+                            appendNode("version", libraryVersions["vungle"])
+                            appendNode("scope", "compile")
+                        }
+                }
+            }
+        }
+    }
 }
