3e4bdb7ce8a11082f78f96585898382c31070aa5
Jian Chen <quantumsix492@gmail.com>
2022-08-12 14:39:54 -0700

    [AND-4218] Build Fixes

--- Vungle/build.gradle.kts
+++ Vungle/build.gradle.kts
@@ -1,3 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://maven.fabric.io/public") }
+        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
+        maven { setUrl("https://repository.jetbrains.com/all") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.library:com.android.library.gradle.plugin:7.2.2")
+        classpath(kotlin("gradle-plugin", version = "1.3.70"))
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
 plugins {
     id("signing")
     id("maven-publish")
@@ -8,6 +26,7 @@ apply(plugin = "com.android.library")
 android {
     defaultConfig {
         compileSdkVersion(31)
+        minSdkVersion(16)
         buildConfigField("int", "VERSION_CODE", "06120001")
         buildConfigField("String", "VERSION_NAME", "\"6.12.0.1\"");
     }
@@ -23,7 +42,6 @@ val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}
 val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
 
 val libraryArtifactId by extra("vungle-adapter")
-val libraryGroupId by extra("com.applovin.mediation")
 
 dependencies {
     implementation("com.vungle:publisher-sdk-android:6.12.0")
