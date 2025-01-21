ed5d87e7deed2782a8c6e7f8b27d29ee1a2e4f05
Fangshuai Hu <fangshuai.hu@vungle.com>
2025-01-21 16:30:48 +0800

    743 dev

--- Vungle/build.gradle.kts
+++ Vungle/build.gradle.kts
@@ -1,9 +1,72 @@
+//plugins {
+//    id("adapter-config")
+//}
+//
+//afterEvaluate {
+//    apply(plugin = "adapter-publish")
+//}
+//
+//val libraryVersionName by extra("7.4.2.0")
+
 plugins {
-    id("adapter-config")
+    id("signing")
+    id("maven-publish")
 }
 
-afterEvaluate {
-    apply(plugin = "adapter-publish")
+private val versionMajor = 7
+private val versionMinor = 4
+private val versionPatch = 3
+private val versionAdapterPatch = 0
+
+val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
+val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
+
+val libraryArtifactId by extra("vungle-adapter")
+val libraryGroupId by extra("com.applovin.mediation")
+
+var libraryVersions = rootProject.extra["versions"] as Map<*, *>
+
+android.defaultConfig.versionCode = libraryVersionCode
+android.defaultConfig.versionName = libraryVersionName
+
+dependencies {
+    implementation("com.vungle:vungle-ads:${libraryVersions["vungle"]}")
 }
 
-val libraryVersionName by extra("7.4.3.0")
+publishing {
+    publications {
+        create<MavenPublication>(extra["publicationName"] as String) {
+            //The publication doesn't know about our dependencies, so we have to manually add them to the pom
+            pom.withXml {
+                asNode().apply {
+                    appendNode("name", libraryArtifactId)
+                    appendNode("description", "Vungle adapter for AppLovin MAX mediation")
+                    appendNode("url", "https://www.applovin.com/")
+                    appendNode("licenses")
+                        .appendNode("license").apply {
+                            appendNode("name", "AppLovin Corporation Mediation Adapter EULA")
+                            appendNode("url", "https://www.applovin.com/eula")
+                        }
+                    appendNode("scm").apply {
+                        appendNode("connection", "scm:git:github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
+                        appendNode("developerConnection", "scm:git:ssh://github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
+                        appendNode("url", "https://github.com/AppLovin/AppLovin-MAX-SDK-Android")
+                    }
+                    appendNode("developers")
+                        .appendNode("developer").apply {
+                            appendNode("name", "AppLovin")
+                            appendNode("url", "https://www.applovin.com")
+                        }
+                    // Add Vungle to list of dependencies.
+                    appendNode("dependencies")
+                        .appendNode("dependency").apply {
+                            appendNode("groupId", "com.vungle")
+                            appendNode("artifactId", "vungle-ads")
+                            appendNode("version", libraryVersions["vungle"])
+                            appendNode("scope", "compile")
+                        }
+                }
+            }
+        }
+    }
+}
